6d317cef1105ab6345cc5c07d2fc5c762e469498
Adrian Chung <36159685+adrianschung@users.noreply.github.com>
2019-02-27 14:50:10 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Quote.create(saying: 'Sample quote', author: 'Sample author')
